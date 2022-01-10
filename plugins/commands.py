84a8047d464aa13ac1d56d5fe1f6cc581ee6c3e0
Captain Jack sparrow <90381786+Captainjacksparrow0433@users.noreply.github.com>
2022-01-10 17:15:09 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,9 +22,9 @@ async def start(client, message):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 Search 🔍', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🎥 Group', url='https://t.me/malayalammoviesmms')
+            InlineKeyboardButton('🎥 Group', url='https://t.me/filmboxcommunity')
             ],[
-            InlineKeyboardButton('📂 Channel', callback_data='help'),
+            InlineKeyboardButton('📂 Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
